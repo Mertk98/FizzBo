e72f2174976d42e718a18d1d694114ed05c1c758
Mertk98 <mertkilic27@icloud.com>
2023-03-13 23:47:00 -0700

    Some little tweaks has been done to run the app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+const propertyRouter = require('./routes/propertyRoutes');
+
 const app = express();
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use('/api/v1/properties', propertyRouter);
 
 module.exports = app;
