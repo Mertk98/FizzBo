0c120e42326e2f9a7f238e99d89152b17275eb60
Mertk98 <mertkilic27@icloud.com>
2023-04-07 01:31:18 -0700

    Simplified the data model

--- backend/models/adModels/propertyModels/buildingModel.js
+++ backend/models/adModels/propertyModels/buildingModel.js
@@ -4,24 +4,13 @@ const Base = require('./basePropertyModel');
 const BuildingSchema = Base.discriminator(
   'Building',
   new mongoose.Schema({
-    adType: {
-      type: String,
-      required: true,
-      enum: ['for sale', 'rental', 'wanted'],
-      deafult: 'for sale',
-    },
     floor: String,
     unitsPerStorey: String,
-    heatingType: String,
-    Features: {
-      elevator: Boolean,
-      security: Boolean,
-      undergroundParking: Boolean,
-      emergencyStairway: Boolean,
-      Amenities: Boolean,
-      Garden: Boolean,
-      PowerSupply: Boolean,
+    heatingType: {
+      type: String,
+      enum: ['Furnace', 'Boiler', 'Heat pump', 'Gas', 'Electric', 'Fireplace'],
     },
+    Features: [String],
   })
 );
 
