51d3123dcbd0d07fe33ebd0bc4bc25ca8cb0f362
Mertk98 <mertkilic27@icloud.com>
2023-04-07 01:38:12 -0700

    Created the housing model

--- backend/models/adModels/propertyModels/housingModel.js
+++ backend/models/adModels/propertyModels/housingModel.js
@@ -1,2 +1,28 @@
 const mongoose = require('mongoose');
 const Base = require('basePropertyModel');
+
+const HousingSchema = Base.discriminator(
+  'Housing',
+  new mongoose.Schema({
+    bathrooms: String,
+    balcony: Boolean,
+    furnished: Boolean,
+    heatingType: {
+      type: String,
+      enum: ['Furnace', 'Boiler', 'Heat pump', 'Gas', 'Electric', 'Fireplace'],
+    },
+    facing: [String],
+    intFeatures: [String],
+    extFeatures: [String],
+    transportation: [String],
+    view: [String],
+    housingType: {
+      type: String,
+      enum: ['House', 'Townhouse', 'Apartment', 'Basement'],
+    },
+  })
+);
+
+const Housing = mongoose.model('Housing');
+
+module.exports = Housing;
