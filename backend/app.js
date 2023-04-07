f277eb2ae78c4adc506328f8bcfd93df1f651c25
Mertk98 <mertkilic27@icloud.com>
2023-04-07 03:32:10 -0700

    Added body-parser, cors, helmet, morgan middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const propertyRouter = require('./routes/propertyRoutes');
 
@@ -6,6 +10,10 @@ const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan());
 
 app.use('/api/v1/properties', propertyRouter);
 
