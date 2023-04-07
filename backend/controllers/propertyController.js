aabf670f951d789120441d53fa5b121e6f7a22c7
Mertk98 <mertkilic27@icloud.com>
2023-04-07 01:37:24 -0700

    Fixed the Building controllers and added basic Housing controllers

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -1,6 +1,14 @@
 const Building = require('../models/adModels/propertyModels/buildingModel');
+const Housing = require('../models/adModels/propertyModels/housingModel');
+
 const factoryHandler = require('./factoryController');
 
-exports.getAll = factoryHandler.getAll(Building);
-exports.getByAdNum = factoryHandler.getByAdNum(Building);
-exports.createListing = factoryHandler.createListing(Building);
+// handler functions for buildings
+exports.getAllBuildings = factoryHandler.getAll(Building);
+exports.getBuildingByAdNum = factoryHandler.getByAdNum(Building);
+exports.createBuildingListing = factoryHandler.createListing(Building);
+
+//handler functions for housing
+exports.getAllHousing = factoryHandler.getAll(Housing);
+exports.getHousingByAdNum = factoryHandler.getByAdNum(Housing);
+exports.createHousingListing = factoryHandler.createListing(Housing);
