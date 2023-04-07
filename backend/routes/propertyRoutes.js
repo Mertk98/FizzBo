f0a44463944c42ee6e680b9858572c854e111d70
Mertk98 <mertkilic27@icloud.com>
2023-04-07 01:39:23 -0700

    Fixed the Building routes and added the Housing routes

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -3,11 +3,20 @@ const propertyController = require('../controllers/propertyController.js');
 
 const router = express.Router();
 
+// Routers for buildings
 router
   .route('/building')
-  .get(propertyController.getAll)
-  .post(propertyController.createListing);
+  .get(propertyController.getAllBuildings)
+  .post(propertyController.createBuildingListing);
 
-router.route('/building/:adNumber').get(propertyController.getByAdNum);
+router.route('/building/:adNumber').get(propertyController.getBuildingByAdNum);
+
+// Routers for houses and apartments
+router
+  .route('/housing')
+  .get(propertyController.getAllHousing)
+  .post(propertyController.createHousingListing);
+
+router.route('/housing/:adNumber').get(propertyController.getHousingByAdNum);
 
 module.exports = router;
