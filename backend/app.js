4201f47f2f1aef4d511bc7abe100d7334afdc68f
Mertk98 <mertkilic27@icloud.com>
2023-05-16 05:54:50 -0700

    Added the cookie-session middleware to the app

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-
+const cookieSession = require('cookie-session');
 const propertyRouter = require('./routes/propertyRoutes');
 
 const app = express();
@@ -12,8 +12,15 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:8080' }));
 app.use(morgan());
+app.use(
+  cookieSession({
+    name: 'fizzbo-session',
+    secret: 'COOKIE-SECRET',
+    httpOnly: true,
+  })
+);
 
 app.use('/api/v1/properties', propertyRouter);
 
