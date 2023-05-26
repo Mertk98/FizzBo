e9e1aee1bcb497abf248dd6756ceeaca0ac10cea
Mertk98 <mertkilic27@icloud.com>
2023-05-26 03:11:56 -0700

    Added the basic user routes and signup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const UserController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+// Basic user routes
+router.route('/').get(UserController.getAllUsers);
+router.route('/:id').get(UserController.getUserById);
+
+// Sign-up routers
+router.route('/signup').post(authController.signup);
+//router.route('/login').post();
+
+module.exports = router;
