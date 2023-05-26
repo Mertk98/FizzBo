3881b5490a772c8c0089bfdb6b866ffcadc3cfeb
Mertk98 <mertkilic27@icloud.com>
2023-05-26 02:52:07 -0700

    Minor changes in the create operation

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -7,19 +7,19 @@ const factoryHandler = require('./factoryController');
 // handler functions for buildings
 exports.getAllBuildings = factoryHandler.getAll(Building);
 exports.getBuildingByAdNum = factoryHandler.getByAdNum(Building);
-exports.createBuildingListing = factoryHandler.createListing(Building);
+exports.createBuildingListing = factoryHandler.create(Building);
 
 //handler functions for housing
 exports.getAllHousing = factoryHandler.getAll(Housing);
 exports.getHousingByAdNum = factoryHandler.getByAdNum(Housing);
-exports.createHousingListing = factoryHandler.createListing(Housing);
+exports.createHousingListing = factoryHandler.create(Housing);
 
 //handler functions for land
 exports.getAllLand = factoryHandler.getAll(Land);
 exports.getLandByAdNum = factoryHandler.getByAdNum(Land);
-exports.createLandListing = factoryHandler.createListing(Land);
+exports.createLandListing = factoryHandler.create(Land);
 
 //handler functions for office
 exports.getAllOffice = factoryHandler.getAll(Office);
 exports.getOfficeByAdNum = factoryHandler.getByAdNum(Office);
-exports.createOfficeListing = factoryHandler.createListing(Office);
+exports.createOfficeListing = factoryHandler.create(Office);
